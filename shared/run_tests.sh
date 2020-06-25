d6d96882432829133100574ba9bbbdc4fa7016d3
Roopak Venkatakrishnan <roopak.v@gmail.com>
2020-06-25 03:58:44 -0700

    ok

--- shared/run_tests.sh
+++ shared/run_tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "This script would run shared/* tests. This is run if either client or server is modified";
+echo "This script would run shared/* tests. If modified this runs both client and server flows";
